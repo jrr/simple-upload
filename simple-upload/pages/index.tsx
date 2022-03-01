8b715710c3cfa3a0b46685e7f4afd5ea732d271a
John Ruble <john.ruble@atomicobject.com>
2022-03-01 12:05:26 -0500

    progress bar. remove more unused starter files.

--- simple-upload/pages/index.tsx
+++ simple-upload/pages/index.tsx
@@ -1,52 +1,67 @@
-import React from 'react'
+import React, { useCallback, useState } from "react";
 
+type UploadState =
+  | {
+    status: "empty";
+  }
+  | {
+    status: "in-progress";
+    loaded: number;
+    total: number;
+    percentage: string;
+  }
+  | {
+    status: "error";
+    error: string;
+  };
 
-function submitFormXHR() {
-  console.log("submitting programmatically with XHR()..");
-  const form = document.getElementById("upload-form") as HTMLFormElement;
-  const formData = new FormData(form);
-  console.log(
-    "entries?",
-    Array.from(formData.entries()) //.map(([a, b]) => b.length)
+const Home: React.FC = () => {
+  const [uploadState, setUploadState] = useState<UploadState>({
+    status: "empty",
+  });
+  const progressUpdate = useCallback(
+    (e: ProgressEvent<XMLHttpRequestEventTarget>) => {
+      setUploadState({
+        status: "in-progress",
+        loaded: e.loaded,
+        total: e.total,
+        percentage: `${Math.round((100 * e.loaded) / e.total)}%`,
+      });
+    },
+    []
   );
-  const xhr = new XMLHttpRequest();
-  addListeners(xhr);
-  xhr.open("POST", form.action);
-  xhr.send(formData);
-}
-
-function handleError(e: any) {
-  //   console.log("event?", e);
-  console.log(
-    `2: ${e.type}: computable?=${e.lengthComputable} ${e.loaded}/${e.total} bytes transferred`
+  const handleError = useCallback(
+    (e: ProgressEvent<XMLHttpRequestEventTarget>) => {
+      console.log("Upload error!", e);
+    },
+    []
   );
-}
-function progressUpdate(e: ProgressEvent<XMLHttpRequestEventTarget>) {
-  //   console.log("event?", e);
-  console.log("progress", (e.loaded / e.total) * 100)
-  // console.log(
-  //   `2: ${e.type}: computable?=${e.lengthComputable} ${e.loaded}/${e.total} bytes transferred`
-  // );
-}
 
-function addListeners(xhr: XMLHttpRequest) {
+  const submitForm = useCallback(() => {
+    console.log("submitting programmatically with XHR()..");
+    const form = document.getElementById("upload-form") as HTMLFormElement;
+    const formData = new FormData(form);
+    console.log(
+      "entries?",
+      Array.from(formData.entries()) //.map(([a, b]) => b.length)
+    );
+    const xhr = new XMLHttpRequest();
 
-  xhr.upload.addEventListener("loadstart", progressUpdate);
-  xhr.upload.addEventListener("progress", progressUpdate);
-  xhr.upload.addEventListener("loadend", progressUpdate);
+    xhr.upload.addEventListener("loadstart", progressUpdate);
+    xhr.upload.addEventListener("progress", progressUpdate);
+    xhr.upload.addEventListener("loadend", progressUpdate);
 
-  // xhr.upload.addEventListener("load", handleEvent);
-  xhr.upload.addEventListener("error", handleError);
-  xhr.upload.addEventListener("abort", handleError);
-}
-
-export default function Home() {
+    xhr.upload.addEventListener("error", handleError);
+    xhr.upload.addEventListener("abort", handleError);
+    xhr.open("POST", form.action);
+    xhr.send(formData);
+  }, []);
 
   const responseMessage = false;
   return (
     <div className="page">
       <head>
-        <title>Hello World - Aleph.js</title>
+        <title>Simple Upload</title>
         <link rel="stylesheet" href="../style/index.css" />
       </head>
       <hr />
@@ -65,13 +80,31 @@ export default function Home() {
           <input type="file" name="files" multiple />
           <br />
         </form>
-        <button onClick={submitFormXHR}>Submit (XHR)</button>
+        <button onClick={submitForm}>Submit (XHR)</button>
 
-        {responseMessage &&
+        {responseMessage && (
           <div className="responseMessage"> {{ responseMessage }} </div>
-        }
+        )}
+        {/* <p>status {JSON.stringify(uploadState)}</p> */}
+        {uploadState.status == "in-progress" && (
+          <>
+            <div className="upload-status">{uploadState.percentage}</div>
+            <div>
+              {uploadState.loaded} / {uploadState.total}
+            </div>
+
+            <div className="progress-bar-outer">
+              <div className="progress-bar-inner" style={{
+                height: "24px",
+                width: uploadState.percentage
+              }}></div>
+            </div>
+          </>
+        )}
       </div>
       <hr />
     </div>
-  )
-}
+  );
+};
+
+export default Home;
