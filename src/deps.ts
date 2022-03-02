af4b1fac0abcb26b6340bc4edbee06910ec04a23
John Ruble <john.ruble@atomicobject.com>
2022-03-02 01:38:46 -0500

    cleanup

--- src/deps.ts
+++
@@ -1,7 +0,0 @@
-export { Server } from "https://deno.land/std@0.127.0/http/server.ts";
-export { ensureDir } from "https://deno.land/std@0.127.0/fs/mod.ts";
-
-export type { Form } from "https://deno.land/x/multiparser@0.114.0/mod.ts";
-export { multiParser } from "https://deno.land/x/multiparser@0.114.0/mod.ts";
-
-export { engineFactory } from "https://deno.land/x/view_engine@v1.5.0/mod.ts";
